b9898a9a3f3130b8ac85f647df6c8afa6b706b3d
Saimen-Luo <31783769+Saimen-Luo@users.noreply.github.com>
2019-12-07 14:25:25 +0800

    feat: 允许跨域

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const cors = require('cors')
 
 const schema = require('./graphql/schema')
 
 const app = express()
 
 require('./mongodb/connect')
+
+// 允许跨域
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
